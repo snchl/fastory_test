b36cfa5f477d1be0b4fa3b0a1226ac0558325888
Alexandre Senechal <alexandresenechal@hessautomobile.com>
2022-12-04 15:55:31 +0100

    feat: planet details page

--- client/pages/planets/[uid].tsx
+++ client/pages/planets/[uid].tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { GetServerSideProps, NextPage } from 'next';
 import Planet from '../../types/Planet';
+import { capitalizeString } from '../../utils/format';
 
 type Props = {
   planet: Planet;
@@ -10,7 +11,62 @@ const Planet: NextPage<Props> = ({ planet }) => {
   return (
     <div className='w-full'>
       <div className='flex flex-col items-center w-2/3 mx-auto'>
-        <h1 className='text-5xl font-medium'>{planet.properties.name}</h1>
+        <h1 className='text-5xl font-medium underline'>
+          {planet.properties.name}
+        </h1>
+        <p className='mt-6 text-2xl underline'>Characteristic</p>
+        <table className='w-1/2 mt-3 text-lg text-center'>
+          <tbody>
+            <tr>
+              <th className='border border-sw-yellow'>Diameter</th>
+              <td className='border border-sw-yellow'>
+                {planet.properties.diameter} km
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Gravity</th>
+              <td className='border border-sw-yellow'>
+                {planet.properties.gravity}
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Orbital period</th>
+              <td className='border border-sw-yellow'>
+                {planet.properties.orbital_period} days
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Rotation period</th>
+              <td className='border border-sw-yellow'>
+                {planet.properties.rotation_period} hours
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Climate</th>
+              <td className='border border-sw-yellow'>
+                {capitalizeString(planet.properties.climate)}
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Terrain</th>
+              <td className='border border-sw-yellow'>
+                {capitalizeString(planet.properties.terrain)}
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Surface water</th>
+              <td className='border border-sw-yellow'>
+                {planet.properties.surface_water} %
+              </td>
+            </tr>
+            <tr>
+              <th className='border border-sw-yellow'>Population</th>
+              <td className='border border-sw-yellow'>
+                {planet.properties.population}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
