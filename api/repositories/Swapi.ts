d86806c774eedd640d3e968a2ee4fdca5f79d83d
Alexandre Senechal <alexandresenechal@hessautomobile.com>
2022-12-05 19:24:43 +0100

    feat(api): not found error

--- api/repositories/Swapi.ts
+++ api/repositories/Swapi.ts
@@ -1,4 +1,5 @@
 import Hapi from '@hapi/hapi';
+import { AxiosError } from 'axios';
 import DataType from '../types/DataType';
 import swapi, { SwapiRequest } from '../utils/swapi';
 
@@ -78,13 +79,19 @@ export default {
             return mappedResponse;
           })
           .catch((error) => {
-            // TODO
+            throw error;
           })
       : await swapi
           .fetchUrl(swapiRequests)
           .then((data) => (data[type] ? data[type] : data))
-          .catch((error) => {
-            // TODO
+          .catch((error: AxiosError) => {
+            const errorJSON: any = error.toJSON();
+            if (errorJSON['status'] === 404) {
+              const response = h.response('Not found').code(404);
+              return response;
+            } else {
+              throw error;
+            }
           });
 
     return response;
